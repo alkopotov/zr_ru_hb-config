18b9b9ed740963960b0d1adf534c0aa704b7d323
AlHobb <118611357+alkopotov@users.noreply.github.com>
2026-05-27 11:21:31 +0300

    20250625 Initial

---
+++ zr_hb-configs.js
@@ -0,0 +1,786 @@
+var adfoxBiddersMap = {
+  "segmento": "1935537",
+  "betweenDigital": "1910903",
+  "adriver": "1938326",
+  "relap": "1911232",
+  "adfox_imho-video": "1936603",
+  "adfox_adsmart": "1926629",
+  "sape": "2215524",
+  "Gnezdo": "2622435",
+  "astralab": "2836409",
+  "hybrid": "2923139",
+  "videonow": "3361634",
+  "sparrow": "3361713",
+  "adfox_yandex_adplay-adv": "3363899",
+};
+
+var adUnits = [
+  {
+    "code": "adfox_156680495796483580",
+    "sizes": [
+      [
+        970,
+        250
+      ],
+      [
+        980,
+        250
+      ],
+      [
+        1000,
+        250
+      ],
+      [
+        1030,
+        250
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "segmento",
+        "params": {
+          "placementId": "2345"
+        }
+      },
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "4533906"
+        }
+      },
+      {
+        "bidder": "adriver",
+        "params": {
+          "placementId": "123:zr_970x250"
+        }
+      },
+      {
+        "bidder": "sape",
+        "params": {
+          "placementId": "801083"
+        }
+      },
+      {
+        "bidder": "Gnezdo",
+        "params": {
+          "placementId": "341583"
+        }
+      },
+      {
+        "bidder": "astralab",
+        "params": {
+          "placementId": "6555d3ba5b907dfcc7dca228"
+        }
+      },
+      {
+        "bidder": "hybrid",
+        "params": {
+          "placementId": "65c614057bc72f0fa033d864"
+        }
+      }
+    ]
+  },
+  {
+    "code": "adfox_15379466693177350",
+    "sizes": [
+      [
+        300,
+        600
+      ],
+      [
+        240,
+        400
+      ],
+      [
+        300,
+        300
+      ],
+      [
+        300,
+        500
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "adriver",
+        "params": {
+          "placementId": "123:zr_300x600_top"
+        }
+      },
+      {
+        "bidder": "segmento",
+        "params": {
+          "placementId": "2344"
+        }
+      },
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "4534289"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "Fj09LP4_LtUvVlZW"
+        }
+      },
+      {
+        "bidder": "adfox_imho-video",
+        "params": {
+          "p1": "csyzm",
+          "p2": "hitz"
+        }
+      },
+      {
+        "bidder": "adfox_adsmart",
+        "params": {
+          "p1": "cuiks",
+          "p2": "hhro"
+        }
+      },
+      {
+        "bidder": "sape",
+        "params": {
+          "placementId": "801084"
+        }
+      },
+      {
+        "bidder": "Gnezdo",
+        "params": {
+          "placementId": "341581"
+        }
+      },
+      {
+        "bidder": "astralab",
+        "params": {
+          "placementId": "6555d4205b907dfcc7dca229"
+        }
+      }
+    ]
+  },
+  {
+    "code": "adfox_15313866932211619",
+    "sizes": [
+      [
+        300,
+        600
+      ],
+      [
+        240,
+        400
+      ],
+      [
+        300,
+        300
+      ],
+      [
+        300,
+        500
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "adriver",
+        "params": {
+          "placementId": "123:zr_300x600_down"
+        }
+      },
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "4534294"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "S2ZmHM4Pb5RuWVlZ"
+        }
+      },
+      {
+        "bidder": "adfox_adsmart",
+        "params": {
+          "p1": "cuikv",
+          "p2": "hhro"
+        }
+      },
+      {
+        "bidder": "adfox_imho-video",
+        "params": {
+          "p1": "cqrza",
+          "p2": "hitz"
+        }
+      },
+      {
+        "bidder": "sape",
+        "params": {
+          "placementId": "801086"
+        }
+      }
+    ]
+  },
+  {
+    "code": "adfox_154564342486561151",
+    "sizes": [
+      [
+        300,
+        600
+      ],
+      [
+        240,
+        400
+      ],
+      [
+        300,
+        300
+      ],
+      [
+        300,
+        500
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "428707"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "Yp1iD84OYYhy0v7-"
+        }
+      },
+      {
+        "bidder": "sape",
+        "params": {
+          "placementId": "801085"
+        }
+      },
+      {
+        "bidder": "Gnezdo",
+        "params": {
+          "placementId": "341582"
+        }
+      },
+      {
+        "bidder": "astralab",
+        "params": {
+          "placementId": "6555d4795b907dfcc7dca22a"
+        }
+      },
+      {
+        "bidder": "hybrid",
+        "params": {
+          "placementId": "65c614057bc72f0fa033d863"
+        }
+      },
+      {
+        "bidder": "sparrow",
+        "params": {
+          "placementId": "107342"
+        }
+      },
+
+    ]
+  },
+  {
+    "code": "adfox_155266277443049906",
+    "sizes": [
+      [
+        630,
+        180
+      ],
+      [
+        570,
+        90
+      ],
+      [
+        620,
+        240
+      ],
+      [
+        560,
+        90
+      ],
+      [
+        670,
+        250
+      ],
+      [
+        670,
+        400
+      ],
+      [
+        468,
+        60
+      ],
+      [
+        680,
+        240
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "4534105"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "k2yTD84OSX6Eq-_v"
+        }
+      },
+      {
+        "bidder": "adfox_adsmart",
+        "params": {
+          "p1": "cuikq",
+          "p2": "ul"
+        }
+      },
+      {
+        "bidder": "Gnezdo",
+        "params": {
+          "placementId": "341584"
+        }
+      }
+    ]
+  },
+  {
+    "code": "adfox_155266495370668775",
+    "sizes": [
+      [
+        630,
+        180
+      ],
+      [
+        570,
+        90
+      ],
+      [
+        620,
+        240
+      ],
+      [
+        560,
+        90
+      ],
+      [
+        670,
+        240
+      ],
+      [
+        468,
+        60
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "4534308"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "LG9vZmqrxj3HLi4u"
+        }
+      },
+      {
+        "bidder": "sparrow",
+        "params": {
+          "placementId": "107343"
+        }
+      },
+    ]
+  },
+  {
+    "code": "adfox_157898908152812557",
+    "sizes": [
+      [
+        336,
+        280
+      ],
+      [
+        320,
+        250
+      ],
+      [
+        300,
+        300
+      ],
+      [
+        300,
+        150
+      ],
+      [
+        300,
+        250
+      ],
+      [
+        300,
+        200
+      ],
+      [
+        320,
+        180
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "adriver",
+        "params": {
+          "placementId": "123:zr_300x300_top"
+        }
+      },
+      {
+        "bidder": "segmento",
+        "params": {
+          "placementId": "2342"
+        }
+      },
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "2427273"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "HOMcQYBAf5ZsZVFR"
+        }
+      },
+      {
+        "bidder": "adfox_adsmart",
+        "params": {
+          "p1": "cuikw",
+          "p2": "ul"
+        }
+      },
+      {
+        "bidder": "adfox_imho-video",
+        "params": {
+          "p1": "cqrsj",
+          "p2": "hiua"
+        }
+      },
+      {
+        "bidder": "sape",
+        "params": {
+          "placementId": "801087"
+        }
+      },
+      {
+        "bidder": "Gnezdo",
+        "params": {
+          "placementId": "341585"
+        }
+      },
+      {
+        "bidder": "astralab",
+        "params": {
+          "placementId": "6555d4ea5b907dfcc7dca22b"
+        }
+      }
+    ]
+  },
+  {
+    "code": "adfox_157899230881497965",
+    "sizes": [
+      [
+        336,
+        280
+      ],
+      [
+        320,
+        250
+      ],
+      [
+        300,
+        300
+      ],
+      [
+        300,
+        150
+      ],
+      [
+        300,
+        250
+      ],
+      [
+        300,
+        200
+      ],
+      [
+        320,
+        180
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "adriver",
+        "params": {
+          "placementId": "123:zr_300x300_mid"
+        }
+      },
+      {
+        "bidder": "segmento",
+        "params": {
+          "placementId": "2343"
+        }
+      },
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "2427274"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "Z1JSRZdWyTLIHx8f"
+        }
+      },
+      {
+        "bidder": "adfox_adsmart",
+        "params": {
+          "p1": "cuikx",
+          "p2": "ul"
+        }
+      },
+      {
+        "bidder": "adfox_imho-video",
+        "params": {
+          "p1": "cxldq",
+          "p2": "hiuc"
+        }
+      },
+      {
+        "bidder": "sape",
+        "params": {
+          "placementId": "801088"
+        }
+      },
+      {
+        "bidder": "Gnezdo",
+        "params": {
+          "placementId": "341586"
+        }
+      },
+      {
+        "bidder": "astralab",
+        "params": {
+          "placementId": "6555d5075b907dfcc7dca22c"
+        }
+      },
+      {
+        "bidder": "hybrid",
+        "params": {
+          "placementId": "65c614057bc72f0fa033d862"
+        }
+      },
+      {
+        "bidder": "videonow",
+        "params": {
+          "placementId": "12470879"
+        }
+      },
+      {
+        "bidder": "sparrow",
+        "params": {
+          "placementId": "107344"
+        }
+      },
+      {
+        "bidder": "adfox_yandex_adplay-adv",
+        "params": {
+          "p1": "dihfo",
+          "p2": "jbzl"
+        }
+      }
+    ]
+  },
+  {
+    "code": "adfox_161701499259386524",
+    "sizes": [
+      [
+        336,
+        280
+      ],
+      [
+        320,
+        250
+      ],
+      [
+        300,
+        300
+      ],
+      [
+        300,
+        150
+      ],
+      [
+        300,
+        250
+      ],
+      [
+        300,
+        200
+      ],
+      [
+        320,
+        180
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "adriver",
+        "params": {
+          "placementId": "123:zr_300x300_down"
+        }
+      },
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "2427275"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "_QL9_Tz8t16kAjQ0"
+        }
+      },
+      {
+        "bidder": "adfox_adsmart",
+        "params": {
+          "p1": "cuiky",
+          "p2": "ul"
+        }
+      },
+      {
+        "bidder": "adfox_imho-video",
+        "params": {
+          "p1": "cqrsm",
+          "p2": "hiua"
+        }
+      },
+      {
+        "bidder": "sape",
+        "params": {
+          "placementId": "801089"
+        }
+      },
+      {
+        "bidder": "Gnezdo",
+        "params": {
+          "placementId": "341587"
+        }
+      },
+      {
+        "bidder": "astralab",
+        "params": {
+          "placementId": "6555d52e5b907dfcc7dca22d"
+        }
+      },
+      {
+        "bidder": "videonow",
+        "params": {
+          "placementId": "12469982"
+        }
+      },
+      {
+        "bidder": "sparrow",
+        "params": {
+          "placementId": "107345"
+        }
+      },
+    ]
+  },
+  {
+    "code": "adfox_165529085166642364",
+    "sizes": [
+      [
+        336,
+        280
+      ],
+      [
+        320,
+        250
+      ],
+      [
+        300,
+        300
+      ],
+      [
+        300,
+        150
+      ],
+      [
+        300,
+        250
+      ],
+      [
+        300,
+        200
+      ],
+      [
+        320,
+        180
+      ]
+    ],
+    "bids": [
+      {
+        "bidder": "betweenDigital",
+        "params": {
+          "placementId": "2427276"
+        }
+      },
+      {
+        "bidder": "relap",
+        "params": {
+          "placementId": "eklJM-EgVq1XMzMz"
+        }
+      }
+    ]
+  }
+];
+var syncPixels = [
+  {
+    "bidder": "adfox_imho-video",
+    "pixels": [
+      'https://www.tns-counter.ru/V13a****idsh_ad/ru/CP1251/tmsec=idsh_dis/',
+      'https://www.tns-counter.ru/V13a****idsh_vid/ru/CP1251/tmsec=idsh_sid90682-dis/',
+      'https://mc.yandex.ru/watch/66716692?page-url=90682%3Futm_source=dis_default%26utm_medium=%26utm_campaign=%26utm_content=%26utm_term=&page-ref=[PAGE_URL]',
+      'https://www.tns-counter.ru/V13a****idsh_ad/ru/CP1251/tmsec=idsh_mob/',
+      'https://www.tns-counter.ru/V13a****idsh_vid/ru/CP1251/tmsec=idsh_sid90671-mob/',
+      'https://mc.yandex.ru/watch/66716692?page-url=90671%3Futm_source=mob_default%26utm_medium=%26utm_campaign=%26utm_content=%26utm_term=&page-ref=[PAGE_URL]'
+    ]
+  },
+  {
+    "bidder": "hybrid",
+    "pixels": [
+      'https://www.tns-counter.ru/V13a****idsh_ad/ru/CP1251/tmsec=idsh_dis/',
+      'https://www.tns-counter.ru/V13a****idsh_vid/ru/CP1251/tmsec=idsh_zrru-hbrdis/',
+      'https://mc.yandex.ru/watch/66716692?page-url=zrru%3Futm_source=dis_hybrid_default%26utm_medium=%26utm_campaign=%26utm_content=%26utm_term=&page-ref=[PAGE_URL]',
+      'https://www.tns-counter.ru/V13a****idsh_ad/ru/CP1251/tmsec=idsh_mob/',
+      'https://www.tns-counter.ru/V13a****idsh_vid/ru/CP1251/tmsec=idsh_zrru-hbrmob/',
+      'https://mc.yandex.ru/watch/66716692?page-url=zrru%3Futm_source=mob_hybrid_default%26utm_medium=%26utm_campaign=%26utm_content=%26utm_term=&page-ref=[PAGE_URL]'
+    ]
+  }
+];
+
+var userTimeout = 500;
+
+window.YaHeaderBiddingSettings = {
+  biddersMap: adfoxBiddersMap,
+  adUnits: adUnits,
+  timeout: userTimeout,
+  syncPixels: syncPixels
+};
